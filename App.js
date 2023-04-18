eda93b8b4c8492c4b56e678e160eefb6216b9f8e
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-04-18 10:13:52 +0530

    Episode 3 Live Classes

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 // const heading=document.createElement("h1")
-        // heading.innerHTML="Namaste from JavaScript";
-        // document.getElementById("root").appendChild(heading);
+// heading.innerHTML="Namaste from JavaScript";
+// document.getElementById("root").appendChild(heading);
 
-        // const heading=React.createElement("h1",{},"Hello world");
-        const Heading1=React.createElement("h1",{id:"one"},"Hello1")
-        const Heading2=React.createElement("h1",{id:"two"},"Hello2")
-        const container=React.createElement("div",{},[Heading1,Heading2])
-        const root=ReactDOM.createRoot(document.getElementById("root"));
-        root.render(container);
+// const heading=React.createElement("h1",{},"Hello world");
+// const Heading1=React.createElement("h1",{id:"one"},"Hello1")
+// const Heading2=React.createElement("h1",{id:"two"},"Hello2")
+// const container=React.createElement("div",{},[Heading1,Heading2])
+//
+const Title=()=>(
+        <h1>Namaste</h1>
+)
+const Heading = () => {
+  return <div>
+        <Title/>
+        Hello World
+        </div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>);
