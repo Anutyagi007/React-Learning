72347834505809126a790c60270335adc8294cc2
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2023-04-23 22:16:16 +0530

    Update

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading=document.createElement("h1")
-// heading.innerHTML="Namaste from JavaScript";
-// document.getElementById("root").appendChild(heading);
-
-// const heading=React.createElement("h1",{},"Hello world");
-// const Heading1=React.createElement("h1",{id:"one"},"Hello1")
-// const Heading2=React.createElement("h1",{id:"two"},"Hello2")
-// const container=React.createElement("div",{},[Heading1,Heading2])
-//
-const Title=()=>(
-        <h1>Namaste</h1>
-)
-const Heading = () => {
-  return <div>
-        <Title/>
-        Hello World
-        </div>;
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        src="https://i.pinimg.com/originals/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg"
+        alt="logo"
+        className="logo"
+      />
+    </a>
+  );
 };
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Sign In</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard=()=>{
+        
+}
+const Body=()=>{
+        return(
+                <div>
+                        Body
+                </div>
+        )
+}
+const Footer=()=>{
+        return(
+                <div>
+                        Footer
+                </div>
+        )
+}
+const AppLayout=()=>{
+        return(
+                <>
+                <HeaderComponent/>
+                <Body/>
+                <Footer/>
+                </>
+        )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>);
+root.render(<AppLayout />);
